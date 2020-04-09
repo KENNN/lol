fc5fea1ff1b21232736696cfa02254120a7aaf5e
Kentaro Yoshida <k@KENNs-MBA11.local>
2020-04-09 21:43:35 +0900

    add: error handler

--- 13_web-server/server.lisp
+++ 13_web-server/server.lisp
@@ -1,5 +1,10 @@
-(error "foo")
+(define-condition foo () ()
+    (:report (lambda (condition stream)
+        (princ "Stop Fooing around, numbskull!" stream))))
 
-(define-condition foo() ()
-    (:report (lambda (condition stream))
-        (princ "Stop Fooing around, numbskull!", stream)))
+(defun bad-function ()
+    (error 'foo))
+
+(handler-case (bad-function)
+    (foo () "somebody signaled foo!")
+    (bar () "somebody signaled bar!"))
